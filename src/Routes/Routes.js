33cf0f15ff2d3b50daad4340745159f1be3ed3d6
R7558 <rupsanaworld1004@gmail.co>
2023-05-28 02:25:36 +0600

    added orders

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { Route,Routes } from 'react-router-dom';
 import Dashboard from '../components/Dashboard/Dashboard';
 import Inventory from '../components/Inventory/Inventory';
+import Orders from '../components/Orders/Orders';
 
 function AppRoutes()  {
     return (
         <Routes>
                 <Route path="/" element={<Dashboard />}></Route>
                <Route path="/inventory" element={<Inventory />}></Route>
-              {/* <Route path="/orders" element={<Orders />}></Route>
-              <Route path="/customers" element={<Customers />}></Route>    */}
+             <Route path="/orders" element={<Orders />}></Route>
+              {/* <Route path="/customers" element={<Customers />}></Route>     */}
             </Routes>
     );
 };

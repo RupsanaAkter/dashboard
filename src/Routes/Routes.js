7b75fa15a6d79fc77e779244fb287541cab34164
R7558 <rupsanaworld1004@gmail.co>
2023-05-28 01:57:05 +0600

    added footer

---
+++ src/Routes/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+
+function AppRoutes()  {
+    return (
+        <Routes>
+               {/* <Route path="/" element={<Dashboard />}></Route>
+              <Route path="/inventory" element={<Inventory />}></Route>
+              <Route path="/orders" element={<Orders />}></Route>
+              <Route path="/customers" element={<Customers />}></Route>  */}
+            </Routes>
+    );
+};
+
+export default AppRoutes;

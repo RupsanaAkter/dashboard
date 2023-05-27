1aaa72a42e9e033ff45bf79b41d4f87ca09b4c03
R7558 <rupsanaworld1004@gmail.co>
2023-05-28 02:20:54 +0600

    added dashboard

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Dashboard from '../components/Dashboard/Dashboard';
 
 function AppRoutes()  {
     return (
         <Routes>
-               {/* <Route path="/" element={<Dashboard />}></Route>
-              <Route path="/inventory" element={<Inventory />}></Route>
+                <Route path="/" element={<Dashboard />}></Route>
+              {/* <Route path="/inventory" element={<Inventory />}></Route>
               <Route path="/orders" element={<Orders />}></Route>
-              <Route path="/customers" element={<Customers />}></Route>  */}
+              <Route path="/customers" element={<Customers />}></Route>   */}
             </Routes>
     );
 };
